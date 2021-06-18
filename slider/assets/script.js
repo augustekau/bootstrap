a46bb5a5b65335e036d859b4e2d04b48754b68b5
augustekau <83609317+augustekau@users.noreply.github.com>
2021-06-18 20:34:11 +0300

    initial commit

---
+++ slider/assets/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $(".first-slider").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  });
+});
+
+$(document).ready(function () {
+  $(".second-slider").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  });
+});
+
+$(document).ready(function () {
+  $(".third-slider").slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+  });
+});
+
+$(document).ready(function () {
+  $(".fourth-slider").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+});
